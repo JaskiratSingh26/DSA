ecdb4a2e39dea8c51b08eed3513ad0d15e9a40e5
jaskirat singh <jaskirattt4@gmail.com>
2025-02-02 19:12:12 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (57.90%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let ans=-1
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==target){
+            return i
+        }
+    }
+    return ans 
+};
