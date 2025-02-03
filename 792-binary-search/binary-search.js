f3b444ae74b995f6191e0b76410ca46bc476d449
jaskirat singh <jaskirattt4@gmail.com>
2025-02-03 17:19:38 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (81.37%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,59 +4,36 @@
  * @return {number}
  */
 var search = function(nums, target) {
-//     if (nums.length === 0) { // Handle empty array case
-//         return -1;
-//     }
+ 
 
 
 
-//    if(nums.length==1 && nums[0]==target){
-//     return 0
-//    }
+ 
 
-// let low=0
-// let high=nums.length 
+let low=0
+let high=nums.length -1
 
-// let mid=0
+let mid=0
 
-// while(low<=high){
-//     let mid=(low+high)/2
-//     if(nums[mid]==target){
-//         return mid
-//     }
-
-//     else if(nums[mid]<target){
-//         low=mid+1
-//     }
-
-// else{
-//     high=mid-1  
-// }
-// }
-
-//  return -1
-
-
-  if (nums.length === 0) {
-        return -1;
+while(low<=high){
+    let mid=Math.floor((low+high)/2)
+    if(nums[mid]==target){
+        return mid
     }
 
-    let low = 0;
-    let high = nums.length - 1; // Correct high index
-
-    while (low <= high) { // The <= is the problem!
-        let mid = Math.floor((low + high) / 2);
-
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] < target) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
+    else if(nums[mid]<target){
+        low=mid+1
     }
 
-    return -1;
+else{
+    high=mid-1  
+}
+}
+
+ return -1
+
+
+
 
 
 };
