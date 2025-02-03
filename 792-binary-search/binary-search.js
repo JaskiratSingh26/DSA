13a8848cfa9a8e13d26c0d00ec035c595c6becca
jaskirat singh <jaskirattt4@gmail.com>
2025-02-03 17:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 52.7 MB (75.06%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,11 +4,59 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let ans=-1
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]==target){
-            return i
+//     if (nums.length === 0) { // Handle empty array case
+//         return -1;
+//     }
+
+
+
+//    if(nums.length==1 && nums[0]==target){
+//     return 0
+//    }
+
+// let low=0
+// let high=nums.length 
+
+// let mid=0
+
+// while(low<=high){
+//     let mid=(low+high)/2
+//     if(nums[mid]==target){
+//         return mid
+//     }
+
+//     else if(nums[mid]<target){
+//         low=mid+1
+//     }
+
+// else{
+//     high=mid-1  
+// }
+// }
+
+//  return -1
+
+
+  if (nums.length === 0) {
+        return -1;
+    }
+
+    let low = 0;
+    let high = nums.length - 1; // Correct high index
+
+    while (low <= high) { // The <= is the problem!
+        let mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
-    return ans 
+
+    return -1;
+
+
 };
