6adcba84efe7514edf7c3d6fabd19340d1aba0de
jaskirat singh <jaskirattt4@gmail.com>
2025-02-26 19:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (6.94%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,16 +4,50 @@
  * @return {number}
  */
 var search = function(nums, target) {
+    
+    let low=0
+    let high=(nums.length)-1
+    let ans=-1
+
+    while(low<=high){
+        let mid=Math.floor((low+high)/2)
+
+         
+        if(nums[mid]==target){
+            ans=mid
+            return ans 
+        } 
 
 
+    else if(nums[low]<=nums[mid]){
+        //left side sorted 
+        if(nums[low]<=target && target<=nums[mid]){
+            high=mid-1
+            //left side serching
 
+        }
+        else{
 
-let ans=-1
-for(let i=0;i<nums.length;i++){
-    if(nums[i]==target){
-        ans=i
+low=mid+1
+//right side  inisde left part searching
+        }
+
     }
-}
+
+    else {
+        //right side sorted 
+        if(nums[mid]<target && target <=nums[high]){
+            low=mid+1
+        }
+        else
+        {
+            high=mid-1
+        }
+    }
+
+    }
+
+
 
 return ans 
 };
