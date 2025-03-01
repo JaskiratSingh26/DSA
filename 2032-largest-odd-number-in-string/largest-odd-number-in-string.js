f6f911f5634439b8781b77aa7ada7207af8e2dca
jaskirat singh <jaskirattt4@gmail.com>
2025-03-01 18:52:19 +0530

    Time: 35 ms (5.10%) | Memory: 73.7 MB (5.10%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function(num) {
+    let data=num.split('')
+     let ans ='noans';
+     let array=[]
+
+     for(let i=(data.length)-1;i>=0;i--){
+
+        if(data[i]%2==1){
+            ans=i
+            break
+        }
+        
+      
+     }
+  
+
+if(ans=='noans'){
+    return ""
+}
+else{
+    for(let i=0;i<=ans;i++){
+    array.push(data[i])
+
+}
+  return array.join('')
+  
+}
+
+
+    
+};
