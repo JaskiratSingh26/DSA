aaaa33e6343a87974d2b5fb9900408804c46c14d
jaskirat singh <jaskirattt4@gmail.com>
2025-03-07 20:46:06 +0530

    Time: 17 ms (90.01%) | Memory: 77.5 MB (26.12%) - LeetSync

---
+++ 148-sort-list/sort-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+
+    let arr = []
+    let temp = head
+    while (temp) {
+        arr.push(temp.val)
+        temp = temp.next
+    }
+
+
+ arr.sort((a, b) => a - b);
+
+
+
+    temp = head
+    for (let i = 0; i < arr.length; i++) {
+        temp.val = arr[i]
+        temp = temp.next
+    }
+
+    return head
+};
