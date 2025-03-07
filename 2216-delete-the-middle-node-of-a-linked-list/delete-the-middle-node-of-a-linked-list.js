a507f551181938e512fa80ecbfae97d78cb3ba2e
jaskirat singh <jaskirattt4@gmail.com>
2025-03-07 20:33:15 +0530

    Time: 3 ms (81.53%) | Memory: 99.6 MB (13.41%) - LeetSync

---
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteMiddle = function (head) {
+if(head.next==null) return null
+    let slow = head
+    let fast = head
+    let prev = null
+    while (fast && fast.next) {
+        prev=slow
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+if(prev){
+
+    prev.next=slow.next
+    return head 
+}
+else{
+ head.next=null
+ return head
+}
+
+
+
+
+
+};
