439d3cc253eb05dffd3ffed1655dd52b1f26ddeb
jaskirat singh <jaskirattt4@gmail.com>
2025-03-07 18:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (6.10%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.js
+++ 328-odd-even-linked-list/odd-even-linked-list.js
@@ -9,33 +9,19 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var oddEvenList = function (head) {
-    let list = []
-    if(head==null|| head.next==null) return head
-    let temp = head
-    while (temp && temp.next) {
-        list.push(temp.val)
-        temp = temp.next.next
+var oddEvenList = function(head) {
+    
+    if(head==null || head.next==null) return head
+
+    let odd=head
+    let even=head.next
+    let evenhead=head.next
+    while(odd.next&& even.next){
+        odd.next=even.next
+        odd=even.next
+        even.next=odd.next
+        even=odd.next
     }
-    if (temp) list.push(temp.val)
-
-    temp = head.next
-
-    while (temp && temp.next) {
-        list.push(temp.val)
-        temp = temp.next.next
-    }
-
-    if (temp) list.push(temp.val)
-
-    temp = head
-    for (let i = 0; i < list.length; i++) {
-        temp.val = list[i]
-        temp = temp.next
-    }
-
-    return head
-
-
-
+    odd.next=evenhead
+    return head 
 };
