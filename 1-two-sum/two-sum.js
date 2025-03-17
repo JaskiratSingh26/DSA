697dab3c4954ac9aae64b143d08580acd2e6bc55
jaskirat singh <jaskirattt4@gmail.com>
2025-03-17 11:22:49 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (21.82%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,19 +3,24 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
- 
- for(let i=0 ;i<nums.length;i++ ){
+var twoSum = function(nums, target) {
 
-  for(let j=i+1 ;j<nums.length;j++){
-   if(nums[i]+nums[j]==target){
-    return [i,j]
+
+  let hash=new Map()
+  let looking=0
+
+  for (let i=0;i<nums.length;i++){
+   looking=target-nums[i]
+   if(hash.has(looking)){
+    let data=hash.get(looking)
+
+    let arr=[i,data]
+    arr.sort()
+    return arr 
    }
 
-  }
-
-
- }
-
-
+   else{
+    hash.set(nums[i],i)
+   }
+  }  
 };
