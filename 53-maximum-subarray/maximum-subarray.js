246a362f13ba6421ad888d06523056e70b0b1c13
jaskirat singh <jaskirattt4@gmail.com>
2025-03-17 14:56:43 +0530

    Time: 2 ms (65.04%) | Memory: 63.5 MB (37.25%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+
+
+    let max = -Infinity;
+   let sum=0
+
+   for(let i=0 ;i<nums.length;i++){
+    sum+=nums[i]
+
+
+    if(max<=sum) max=sum
+
+
+    if(sum<=0) sum=0
+   }
+
+
+   return max
+
+
+
+
+
+
+    
+
+
+};
