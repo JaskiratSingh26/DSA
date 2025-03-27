d41eeb38bcc89c763971cd6ba98a1022fb9c5cd7
jaskirat singh <jaskirattt4@gmail.com>
2025-03-27 19:56:24 +0530

    Time: 3 ms (55.22%) | Memory: 56.3 MB (26.42%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let arr = [];
+
+    for (let v of s) {
+        if (v === '(') {
+            arr.push(v);
+        } else if (v === '{') {
+            arr.push(v);
+        } else if (v === '[') {
+            arr.push(v);
+        } else if (v === ')') {
+            if (arr.length > 0 && arr[arr.length - 1] === '(') {
+                arr.pop();
+            } else {
+                return false;
+            }
+        } else if (v === ']') {
+            if (arr.length > 0 && arr[arr.length - 1] === '[') {
+                arr.pop();
+            } else {
+                return false;
+            }
+        } else if (v === '}') {
+            if (arr.length > 0 && arr[arr.length - 1] === '{') {
+                arr.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return arr.length === 0;
+};
