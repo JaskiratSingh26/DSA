ca07081e0791facaebefe3b705af25901501c539
jaskirat singh <jaskirattt4@gmail.com>
2025-03-31 20:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (31.92%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -11,24 +11,49 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function (head, n) {
-    let fast = head
+ 
+ let cur=head
+ let arr=[]
+while(cur){
 
-    let slow = head
-    for (let i = 0; i < n; i++) {
-        if (!fast) return head; // Handle n being larger than list length
-        fast = fast.next;
-    }
+    arr.push(cur)
+    cur=cur.next
+}
 
-    if (!fast) return head.next; // Handle removing the head node
+  let index = arr.length - n;
 
-    while (fast.next) {
-        slow = slow.next;
-        fast = fast.next;
-    }
+if(arr.length==1 && n==1){
+    return null
+}
 
-    slow.next = slow.next.next;
+if(index==0){
+    return head.next
+}
 
-    return head;
+let prev=arr[index -1]
+
+prev.next=prev.next.next
+return head
+
+//    let fast = head
+
+//     let slow = head
+//     for (let i = 0; i < n; i++) {
+//         if (!fast) return head; // Handle n being larger than list length
+//         fast = fast.next;
+//     }
+
+//     if (!fast) return head.next; // Handle removing the head node
+
+//     while (fast.next) {
+//         slow = slow.next;
+//         fast = fast.next;
+//     }
+
+//     slow.next = slow.next.next;
+
+//     return head;
+
 
 
 };
