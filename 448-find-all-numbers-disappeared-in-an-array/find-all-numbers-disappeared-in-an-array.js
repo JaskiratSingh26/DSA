f095c5d26dca9a2098b12482ad00fd3edb7e56f2
jaskirat singh <jaskirattt4@gmail.com>
2025-04-12 19:11:38 +0530

    Time: 6 ms (91.30%) | Memory: 71.5 MB (76.18%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+
+    let ans = []
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i])-1
+
+        if (nums[index] >= 0) {
+
+            nums[index] *= -1
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            let realindex = i + 1
+            ans.push(realindex)
+        }
+    }
+    return ans
+};
