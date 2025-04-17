e5ecae8d488a38a07150a3f488edb61e1165c68c
jaskirat singh <jaskirattt4@gmail.com>
2025-04-17 20:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 57 MB (92.88%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.js
+++ 328-odd-even-linked-list/odd-even-linked-list.js
@@ -9,19 +9,19 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var oddEvenList = function(head) {
-    
-    if(head==null || head.next==null) return head
+var oddEvenList = function (head) {
+ if(head==null || head.next==null) return head
+    let odd = head
+    let even = head.next
+    let temp = head.next
+    while (odd.next && even.next) {
+        odd.next = even.next
+        odd = even.next
+        even.next = odd.next
+        even = odd.next
 
-    let odd=head
-    let even=head.next
-    let evenhead=head.next
-    while(odd.next&& even.next){
-        odd.next=even.next
-        odd=even.next
-        even.next=odd.next
-        even=odd.next
+    
     }
-    odd.next=evenhead
-    return head 
+    odd.next = temp
+    return head
 };
