c0f7721dea913bf42259b46f2b5680682c056a3d
jaskirat singh <jaskirattt4@gmail.com>
2025-04-17 19:52:34 +0530

    Time: 2 ms (100.00%) | Memory: 102.8 MB (10.52%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.js
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.js
@@ -11,26 +11,24 @@
  */
 var deleteMiddle = function (head) {
 
-    if(head.next==null){
-        return null
+   if(head.next==null) return null
+    let slow = head
+    let fast = head
+    let prev = null
+    while (fast && fast.next) {
+        prev=slow
+        slow = slow.next
+        fast = fast.next.next
     }
 
-let slow=head
-let fast=head
+if(prev){
 
-while(fast && fast.next){
-   slow=slow.next
-   fast=fast.next.next
+    prev.next=slow.next
+    return head 
 }
-
-let bypassNode=head
-
-while(bypassNode.next){
-    if(bypassNode.next==slow){
-        bypassNode.next=slow.next
-        return head
-    }
-    bypassNode=bypassNode.next
+else{
+ head.next=null
+ return head
 }
 
 
