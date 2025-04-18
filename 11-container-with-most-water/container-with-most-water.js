ab7f4f90bcbc453350f458d03ee45b82c9486f8d
jaskirat singh <jaskirattt4@gmail.com>
2025-04-18 17:41:57 +0530

    Time: 3 ms (55.39%) | Memory: 63 MB (54.39%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -2,33 +2,31 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {
-    let mostwater = 0
+var maxArea = function(height) {
+    let w=0
+    let h=0
+    let l=0
+    let r=height.length-1
+    let mostwater=0
+    let area=0
 
-    let w = 0
-    let h = 0
-    let area = 1
-    let leftpointer=0
-    let rightpointer=(height.length)-1
 
-    while(leftpointer<rightpointer){
-        
-        w=rightpointer-leftpointer
-        h=Math.min(height[leftpointer],height[rightpointer])
+
+    while(l<r){
+        w=r-l
+        h=Math.min(height[l],height[r])
         area=w*h
+
         mostwater=Math.max(mostwater,area)
 
-        if(height[leftpointer]<height[rightpointer]) {
-            leftpointer++
+
+        if(height[l]<height[r]){
+            l++
         }
         else{
-            rightpointer--
+            r--
         }
-
-
     }
 
     return mostwater
-   
-
 };
