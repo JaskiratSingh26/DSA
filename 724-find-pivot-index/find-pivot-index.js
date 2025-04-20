5947e9798e2dd153f32b14841d1b161db0f564e1
jaskirat singh <jaskirattt4@gmail.com>
2025-04-20 11:15:58 +0530

    Time: 3 ms (50.07%) | Memory: 54.9 MB (99.69%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let left=0
+    let right=0
+
+    for(let i=0;i<nums.length;i++){
+        right+=nums[i]
+    }
+
+
+ for(let i=0;i<nums.length;i++){
+    if(left==right-nums[i]){
+        return i
+    }
+
+
+    left +=nums[i]
+    right-=nums[i]
+    
+ }
+
+    return -1
+
+};
