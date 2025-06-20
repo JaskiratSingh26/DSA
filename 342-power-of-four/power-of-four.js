7be66ea27ddbd8c9e0fc885b74cb3d8d9e7bb008
jaskirat singh <jaskirattt4@gmail.com>
2025-06-20 13:16:19 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (8.93%) - LeetSync

--- 342-power-of-four/power-of-four.js
+++ 342-power-of-four/power-of-four.js
@@ -2,12 +2,12 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
-    if(n<1) return false 
 
-    while(n%4==0){
-        n=n/4
-    }
 
-    return n ==1
+var isPowerOfFour = function (n) {
+
+    if (n === 1) return true;         // Base case: 3^0 = 1
+    if (n < 1 || n % 4 !== 0) return false; // If not divisible by 3, not a power
+
+    return isPowerOfFour(n / 4)
 };
