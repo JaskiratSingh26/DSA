0ad46cd8e44a187bb65738a9ac1863107d3858d6
jaskirat singh <jaskirattt4@gmail.com>
2025-06-20 11:02:04 +0530

    Time: 2 ms (51.76%) | Memory: 57.9 MB (26.70%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -2,30 +2,34 @@
  * @param {string} s
  * @return {number}
  */
+
 var myAtoi = function (s) {
-    const min = -(2 ** 31);
-    const max = 2 ** 31 - 1;
+    let min = -(2 ** 31)
+    let max = 2 ** 31 - 1
     let result = 0;
     let sign = 1;
 
     s = s.trim();
-    let i = 0;
 
-    if (s[i] === '-') {
+    let i = 0;
+   if (s[0] === '-') {
         sign = -1;
-        i++;
-    } else if (s[i] === '+') {
-        i++;
+        i = 1;
+    } else if (s[0] === '+') {
+        i = 1;
     }
+
 
     for (; i < s.length; i++) {
-        if (s[i] < '0' || s[i] > '9') break;
+        if (s[i] < '0' || s[i] > '9') {
+            break;
+        } else {
+            result = result * 10 + (s[i] - '0');
 
-        result = result * 10 + (s[i] - '0');
-
-        if (sign * result <= min) return min;
-        if (sign * result >= max) return max;
+            if (sign * result <= min) return min
+            if (sign * result >= max) return max
+        }
     }
 
-    return sign * result;
+    return sign === -1 ? -result : result;
 };
