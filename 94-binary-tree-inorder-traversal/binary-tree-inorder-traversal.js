c80307b45dbefd15b7804bab9393b8b22111502f
jaskirat singh <jaskirattt4@gmail.com>
2025-06-20 15:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (42.62%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+
+    if (!root) return[]
+    let stack = []
+    let node = root
+    let result = []
+    while (true) {
+        if (node) {
+            stack.push(node)
+            node = node.left
+        }
+        else {
+            if (stack.length == 0) break
+            node = stack.pop()
+            result.push(node.val)
+            node = node.right
+
+        }
+    }
+
+
+    return result
+
+};
