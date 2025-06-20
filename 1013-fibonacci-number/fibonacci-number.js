ec04753b699191435a0d9b6088a296920f101947
jaskirat singh <jaskirattt4@gmail.com>
2025-06-20 14:14:17 +0530

    Time: 65 ms (27.60%) | Memory: 53.8 MB (27.94%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,18 +3,8 @@
  * @return {number}
  */
 var fib = function (n) {
-    let a = 0
-    let b = 1
-    if (n == 0) return 0
-    else if (n == 1) return 1
+if (n === 0) return 0;
+    if (n === 1) return 1;
+    return fib(n - 1) + fib(n - 2);
 
-    else {
-        for (let i = 1; i < n; i++) {
-            let c = a + b
-            a = b
-            b = c
-        }
-
-    }
-    return b
 };
