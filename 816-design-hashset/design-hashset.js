32dbeb7ccc7769b614025a5b5206f717f9ae0624
jaskirat singh <jaskirattt4@gmail.com>
2025-06-28 12:36:48 +0530

    Time: 2777 ms (5.05%) | Memory: 242.3 MB (5.05%) - LeetSync

--- 816-design-hashset/design-hashset.js
+++ 816-design-hashset/design-hashset.js
@@ -1,30 +1,50 @@
-
-var MyHashSet = function() {
-    this.set=new Array(10**6).fill(false)
+let size = 10 ** 6
+function getindex(key) {
+    return key % size
+}
+var MyHashSet = function () {
+    this.buckets = new Array(size).fill(null).map(() => [])
 };
 
 /** 
  * @param {number} key
  * @return {void}
  */
-MyHashSet.prototype.add = function(key) {
-    this.set[key]=true
+MyHashSet.prototype.add = function (key) {
+    let index = getindex(key)
+    let singlebucket = this.buckets[index]
+
+    if (!singlebucket.includes(key)) {
+
+        singlebucket.push(key)
+    }
+
 };
 
 /** 
  * @param {number} key
  * @return {void}
  */
-MyHashSet.prototype.remove = function(key) {
- this.set[key]=false 
+MyHashSet.prototype.remove = function (key) {
+   let index=getindex(key)
+   let singlebucket=this.buckets[index]
+   let keyinbucket=singlebucket.indexOf(key)
+
+  if(keyinbucket!=-1){
+    singlebucket.splice(keyinbucket,1)
+  }
 };
 
 /** 
  * @param {number} key
  * @return {boolean}
  */
-MyHashSet.prototype.contains = function(key) {
-    return this.set[key]
+MyHashSet.prototype.contains = function (key) {
+    let index=getindex(key)
+    let singlebucket=this.buckets[index]
+    return singlebucket.includes(key)
+
+
 };
 
 /** 
