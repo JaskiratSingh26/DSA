b08a41a62d551e36f8a01a75702adf76d6117c23
jaskirat singh <jaskirattt4@gmail.com>
2025-07-02 11:07:30 +0530

    Time: 5 ms (17.25%) | Memory: 57.7 MB (75.81%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -10,29 +10,30 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
- let ans =[]
- let queue=[]
-    if(!root) return ans
-    
-
-    queue.push(root)
-    while(queue.length>0){
-       let subarr=[]
-        let size=queue.length
-
-         for(let i=0;i<size;i++){
-             let node=queue.shift()
-             
-             if(node.left)queue.push(node.left)
-             if(node.right) queue.push(node.right)
-             subarr.push(node.val)
-         }
-
-         ans.push(subarr)
+var levelOrder = function (root) {
 
 
+    let ans = []
+    let queue = []
+    let node = root
+    if (!node) return ans; // ✅ Handle null root
+
+
+
+    queue.push(node)
+    while (queue.length > 0) {
+        let subarr = []
+        let size = queue.length
+
+        for (let i = 0; i < size; i++) {
+            node = queue.shift()
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+            subarr.push(node.val)
+        }
+        ans.push(subarr)
     }
-return ans
+    return ans
+
 };
 
