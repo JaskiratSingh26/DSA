f9f6096d7599ba70f941fb90f401a294c38b5eb3
jaskirat singh <jaskirattt4@gmail.com>
2025-07-02 17:47:21 +0530

    Time: 5 ms (27.27%) | Memory: 54.8 MB (87.88%) - LeetSync

--- 889-buddy-strings/buddy-strings.js
+++ 889-buddy-strings/buddy-strings.js
@@ -7,55 +7,39 @@ var buddyStrings = function (s, goal) {
     if (s.length != goal.length) return false
 
     if (s == goal) {
-        // let map = new Map()
-
-        // for (let i = 0; i < s.length; i++) {
-        //     if (map.has(s[i])) {
-        //         map.set(s[i], map.get(s[i]) + 1)
-        //     }
-
-        //     else {
-        //         map.set(s[i], 1)
-        //     }
-        // }
-
-        // let count=0
-
-        // for(let [key,values] of map){
-        //     if(values>1) count++
-        // }
-
-        // if(count==1) return true
-        // return false
-        let set = new Set(s);
-        return set.size < s.length; 
+        let seen = new Set();
+        for (let char of s) {
+            if (seen.has(char)) return true; // found a duplicate
+            seen.add(char);
+        }
+        return false
 
     }
 
 
-    let index=[]
+    let index = []
 
-    for(let i=0;i<s.length;i++){
-        if(s[i]!=goal[i]) index.push(i)
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] != goal[i]) index.push(i)
     }
 
-    if(index.length==2){
+    if (index.length == 2) {
 
 
-        let sarr=s.split('')
-        let temp=sarr[index[1]]
-        sarr[index[1]]=sarr[index[0]]
-        sarr[index[0]]=temp
+        let sarr = s.split('')
+        let temp = sarr[index[1]]
+        sarr[index[1]] = sarr[index[0]]
+        sarr[index[0]] = temp
 
 
-        if(sarr.join('')==goal) return true
-        return false 
+        if (sarr.join('') == goal) return true
+        return false
 
 
 
     }
 
-    return false 
+    return false
 
 
 };
