7d8e033d6b623eb587f42a8973a2d16e15be8588
jaskirat singh <jaskirattt4@gmail.com>
2025-07-05 13:04:48 +0530

    Time: 21 ms (35.00%) | Memory: 60.7 MB (15.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,39 +3,40 @@
  * @param {string} t
  * @return {boolean}
  */
-        
-    
-   var isAnagram = function(s, t) {
-    let map1 = new Map();
-    let map2 = new Map();
 
-    if (s.length !== t.length) return false;
-    if (s.length <= 1 && s.length > (5 * 10 ** 4)) return false; // This check doesn't make logical sense, but I'll leave it as you provided it.
 
-    for (let i = 0; i < s.length; i++) {
-        if (map1.has(s[i])) {
-            map1.set(s[i], map1.get(s[i]) + 1);
-        } else {
-            map1.set(s[i], 1);
+var isAnagram = function (s, t) {
+    let map1 = new Map()
+    let map2 = new Map()
+
+    if (s.length != t.length) return false
+
+    for (let char of s) {
+        if (map1.has(char)) {
+            map1.set(char, map1.get(char) + 1)
+        }
+        else {
+            map1.set(char, 1)
         }
     }
 
-    for (let i = 0; i < s.length; i++) { // Corrected line
-        if (map2.has(t[i])) {
-            map2.set(t[i], map2.get(t[i]) + 1);
-        } else {
-            map2.set(t[i], 1);
+    for (let char of t) {
+        if (map2.has(char)) {
+            map2.set(char, map2.get(char) + 1)
+        }
+        else {
+            map2.set(char, 1)
         }
     }
 
-    for (let [key, value] of map1) {
+
+
+    for (let [key, values] of  map1) {
+        if (!map2.has(key)) return false
+
         if (map2.has(key)) {
-            if (value !== map2.get(key)) {
-                return false;
-            }
-        } else {
-            return false;
+            if (map2.get(key) != values) return false
         }
     }
-    return true;
+    return true
 };
