ad729a0c3ffc44799bae90c6dd8bfdcfe7c19bc2
jaskirat singh <jaskirattt4@gmail.com>
2025-07-23 11:14:44 +0530

    Time: 3 ms (10.81%) | Memory: 59.2 MB (10.18%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -1,3 +1,4 @@
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -11,29 +12,24 @@
  * @return {number}
  */
 var maxDepth = function (root) {
+    if (!root) return 0
+    let res = []
+    let cur = root
+    let queue = [cur]
 
-    // for doing this what i am doing is that i am checking the lvel roder and in the end it autmolcciy tell the dpeth 
 
-    let ans = []
-    let queue = []
-    let node = root
-    
-    if(node==null) return ans.length
-    queue.push(node)
-    
-
-    while(queue.length>0){
-        let subarr=[]
-        let size=queue.length
-        for(let i=0;i<size;i++){
-            node=queue.shift()
-            if(node.left) queue.push(node.left)
-            if(node.right) queue.push(node.right)
+    while (queue.length != 0) {
+        let n = queue.length
+        let node;
+        let subarr = []
+        for (let i = 0; i < n; i++) {
+            node = queue.shift()
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
             subarr.push(node.val)
         }
-
-        ans.push(subarr)
+        res.push(subarr)
     }
 
-    return ans.length
+    return res.length
 };
