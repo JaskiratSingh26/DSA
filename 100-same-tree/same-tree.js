ec1a26915cb61d7ad77bc96eb02413de2a0d0e8e
jaskirat singh <jaskirattt4@gmail.com>
2025-07-23 11:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (19.36%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -12,18 +12,22 @@
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-    let stack = [[p, q]]
-    while (stack.length > 0) {
-        let [node1, node2] = stack.pop()
-        if (!node1 && !node2) continue; // both null → OK
-        if (!node1 || !node2) return false; // one null → mismatch
-        if (node1.val !== node2.val) return false;
+    let q1 = [p]
+    let q2 = [q]
+
+    while (q1.length!=0 && q2.length!=0) {
+        let node1 = q1.shift()
+        let node2 = q2.shift()
+
+        if (!node1 && !node2) continue
+        if (!node1 || !node2 || node1.val != node2.val) return false
+        q1.push(node1.left);
+        q1.push(node1.right);
+
+        q2.push(node2.left);
+        q2.push(node2.right);
 
 
-
-        stack.push([node1.right, node2.right])
-        stack.push([node1.left, node2.left])
     }
-
-    return true
+    return q1.length==q2.length
 };
