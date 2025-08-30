f0dd764e882cbb18e6a1585a9e942ac1dbaed37a
jaskirat singh <jaskirattt4@gmail.com>
2025-08-30 14:38:33 +0530

    Time: 39 ms (41.03%) | Memory: 71.1 MB (92.61%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -4,38 +4,25 @@
  */
 var threeSum = function (nums) {
     let ans = []
-
     nums.sort((a, b) => a - b)
-
-    for (let i = 0; i < nums.length - 2; i++) {
-
-        let left = i + 1
-        let right = nums.length - 1
-        let subarr = []
-        if (i > 0 && nums[i] == nums[i - 1]) continue
-
-        while (left < right) {
-            let sum = nums[i] + nums[left] + nums[right]
-
-            if (sum < 0) left++
-            if (sum > 0) right--
-
-            if (sum == 0) {
-                subarr = [nums[i], nums[left], nums[right]]
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == nums[i - 1]) continue
+        let j = i + 1
+        let k = nums.length - 1
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k]
+            if (sum < 0) j++
+            else if (sum > 0) k--
+            else {
+                // ehere sum==0
+                let subarr = [nums[i], nums[j], nums[k]]
                 ans.push(subarr)
-                // Skip duplicates
-                while (left < right && nums[left] === nums[left + 1]) left++;
-                while (left < right && nums[right] === nums[right - 1]) right--;
+                j++
+                k--
 
-                left++;
-                right--;
+                while (j < k && nums[j] == nums[j - 1]) j++
             }
-
-
-
         }
-     
     }
-
     return ans
 };
