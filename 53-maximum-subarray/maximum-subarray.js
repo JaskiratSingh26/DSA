dbbe64612ec40d5199120ea72693701976dddac3
jaskirat singh <jaskirattt4@gmail.com>
2025-08-30 14:12:28 +0530

    Time: 1 ms (90.74%) | Memory: 61.7 MB (99.37%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -2,27 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (nums) {
+var maxSubArray = function(nums) {
 
-
-    let max = -Infinity;
-   let sum=0
-
-   for(let i=0 ;i<nums.length;i++){
+  let max=  nums[0]
+  let sum=0
+  for(let i=0;i<nums.length;i++){
     sum+=nums[i]
-
-
-    if(max<=sum) max=sum
-
-
-    if(sum<=0) sum=0
-   }
-
-
-   return max
-  
-
-
-
-
+    if(sum>max) max=sum
+    if(sum<0) sum=0
+  }
+  return max
 };
