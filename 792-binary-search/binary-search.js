146e81b44f480e4a318fc5f5f54dc2cbe9a34bff
jaskirat singh <jaskirattt4@gmail.com>
2026-01-15 12:42:25 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (65.71%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,39 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-
-    if(nums.length==1){
-        if(nums[0]==target){
-            return 0
-        }
-        else {
-            return -1
-        }
-    }
-
     
 
-  let low=0
-  let  high=(nums.length)-1
-    while (low<=high){
-    let mid= Math.floor((low+high)/2)
-     if(nums[mid]==target){
-        return mid
-}
+    let  low=0
+    let high=nums.length-1
 
-else if(nums[mid]<target){
-    low=mid+1
-    
-}
-
-else if(nums[mid]>target){
-
-high=mid-1
-
-}
+    while(low<=high){
+        let mid=Math.floor((low+high)/2)
+        if(nums[mid]==target) return mid
+        else if(nums[mid]>target) high=mid-1
+        else low=mid+1
     }
     return -1
-
-
-    
 };
