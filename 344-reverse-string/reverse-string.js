53d78e6e398f3da05184354698c3fb69229898bd
jaskirat singh <jaskirattt4@gmail.com>
2026-01-16 11:35:22 +0530

    Time: 0 ms (100.00%) | Memory: 61.6 MB (32.22%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -2,17 +2,21 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function (s) {
-    let i = 0
-    let j = s.length - 1
+var reverseString = function(s) {
+    
 
+        let i=0
+        let j=s.length-1
 
-    while (i <= j) {
-       
-       [s[i],s[j]]=[s[j],s[i]]
-       i++
-       j--
-    }
+        while(i<=j){
 
-    return s
+            let temp=s[i]
+            s[i]=s[j]
+            s[j]=temp
+            i++
+            j--
+        
+        }
+        
+        return s
 };
