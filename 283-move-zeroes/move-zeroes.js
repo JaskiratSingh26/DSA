7577d91720b6008b420b9c62eddb89f84cced72c
jaskirat singh <jaskirattt4@gmail.com>
2026-01-17 13:56:34 +0530

    Time: 5 ms (21.31%) | Memory: 60.9 MB (28.09%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -2,26 +2,28 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
+var moveZeroes = function (nums) {
 
-   
 
-let i=0
-let j=0
+    let i = 0
+    let j = i + 1
 
-while(j<nums.length){
-    if(nums[j]!=0){
-        [nums[i],nums[j]]=[nums[j],nums[i]]
-        i++
-        j++
+    while (j < nums.length) {
+        if (nums[j] != 0 && nums[i] == 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+            i++
+            j++
+        }
+        else if (nums[i] != 0) {
+            i++
+            // Ensure j stays ahead of i or moves with it
+            if (j <= i) j = i + 1;
+        }
+        else {
+            j++
+        }
     }
 
-   else{
-    j++
-   }
 
-    
-}
 
- return nums 
 };
